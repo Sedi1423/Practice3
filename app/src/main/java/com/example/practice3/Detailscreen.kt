5597f017c15d7cb27d9f591de7d4f8be09a482e4
Sedi1423 <lesedimhlanga6@gmail.com>
2026-06-09 15:30:23 +0200

    code for first screen update

--- app/src/main/java/com/example/practice3/Detailscreen.kt
+++ app/src/main/java/com/example/practice3/Detailscreen.kt
@@ -11,7 +11,7 @@ class Detailscreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_detailscreen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.txtmain)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
--- app/src/main/java/com/example/practice3/MainActivity.kt
+++ app/src/main/java/com/example/practice3/MainActivity.kt
@@ -1,20 +1,48 @@
 package com.example.practice3
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var nameInput: EditText
+    lateinit var studentnumberInput:EditText
+    lateinit var btnLogin: Button
+    lateinit var btnExit:Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        nameInput=findViewById(R.id.nameInput)
+        studentnumberInput=findViewById(R.id.studentnumberInput)
+        btnLogin=findViewById(R.id.btnLogin)
+        btnExit=findViewById(R.id.btnExit)
+
+        btnExit.setOnClickListener{
+           //closing app
+            finishAffinity()
+
         }
+        btnLogin.setOnClickListener {
+//button to login and go to the next screen
+
+            val intent = Intent(this, MainScreen::class.java)
+            startActivity(intent)
+        
+
+
+
+        }
+
+
     }
 }
